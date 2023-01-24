96991256bd600167346276d88618530b21315916
2021-DEV-068 <macbookbackupnow@gmail.com>
2023-01-24 23:34:44 +0100

    set

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -155,7 +155,6 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = BASE_DIR / 'uploads/'
 MEDIA_URL = 'media/'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
