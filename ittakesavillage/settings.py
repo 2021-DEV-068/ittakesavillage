33245da4491f84313d8b7539634d4fef45785e5b
2021-DEV-068 <94018902+2021-DEV-068@users.noreply.github.com>
2023-01-24 16:04:55 +0100

    Add files via upload

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import environ
+import environ	
 import os
 from pathlib import Path
 
@@ -36,8 +36,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*', 'django-server-production-668b.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app/', 'https://*.127.0.0.1', 'https://django-server-production-668b.up.railway.app/']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app/', 'https://*.127.0.0.1']
 
 if DEBUG:
     import socket  # only if you haven't already imported this
