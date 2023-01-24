3a2a00b49255879108c4dba94cef10628b3259a7
2021-DEV-068 <macbookbackupnow@gmail.com>
2023-01-24 22:20:53 +0100

    set

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -148,8 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / 'public'
 
 # Media
 MEDIA_ROOT = BASE_DIR / 'uploads/'
