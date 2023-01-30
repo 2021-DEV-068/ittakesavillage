3f9167603c05f8d7a3e7edf0ae1c1631f7c7312a
2021-DEV-068 <macbookbackupnow@gmail.com>
2023-01-31 00:17:02 +0100

    sign up

--- website/views/caregiver/membershipcard.py
+++ website/views/caregiver/membershipcard.py
@@ -1,9 +1,12 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse
+
 
 class membershipCardView():
     template_name = "website/caregiver/membershipcard.html"
-    render('caregiver/membershipcard.html')
+    # render('caregiver/membershipcard.html')
+    reverse("caregiver:membershipcard")
     # template_name = "website/caregiver/signup.html"
     # form_class = SignUpForm
     # success_message = _("We will proceed your request.")
