7b90d3a9a41f5405f034c0f0da235467dfb23d7b
2021-DEV-068 <macbookbackupnow@gmail.com>
2023-01-31 00:35:42 +0100

    Updated membershipcard

--- website/views/caregiver/membershipcard.py
+++ website/views/caregiver/membershipcard.py
@@ -1,11 +1,3 @@
 from django.views.generic import TemplateView
-from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
-
-
-class membershipCardView(SuccessMessageMixin):
-    template_name = "website/caregiver/membershipcard.html"
-    success_message = _("We will proceed your request.")
-    def get_success_url(self):
-        return reverse("caregiver:membershipcard")
+class membershipCardView(TemplateView):
+    template_name = "website/membershipcard/home.html"
